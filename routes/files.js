c5913949cc3128a4f3c662fef69f4b4be67819a6
Ahmad Rasheed <ahmadd42@gmail.com>
2026-01-17 15:54:14 +0500

    Allowed authoriation headers

--- routes/files.js
+++ routes/files.js
@@ -36,7 +36,7 @@ router.use(bodyParser.json());
 router.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
