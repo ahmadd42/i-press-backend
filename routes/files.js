b75011206b1c1b1fa192a54ebb74b0229e600921
Ahmad Rasheed <ahmadd42@gmail.com>
2026-03-04 07:39:15 +0500

    Allowed all origins'

--- routes/files.js
+++ routes/files.js
@@ -34,7 +34,7 @@ router.use(bodyParser.json());
 
 // Manual CORS headers middleware
 router.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "gopress.online");
+  res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
