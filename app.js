fbfe056a2895672e3488997f187f717d41219fbc
Ahmad Rasheed <ahmadd42@gmail.com>
2026-06-15 11:20:25 +0500

    Added login rate limit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const app = express();
 const fileRoutes = require("./routes/files");
-
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10
+});
 
 app.use(express.json());
 app.use("/files", fileRoutes);
+app.use("/files/login", loginLimiter);
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
